386e0f64c925bcdc03eb55e37fb9469476b15f67
Agustin Ojeda <agojeda@cartasur.com.ar>
2024-09-15 20:16:38 -0300

    init

---
+++ src/main/java/com/example/ajswact1/AjswAct1Application.java
@@ -0,0 +1,13 @@
+package com.example.ajswact1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjswAct1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjswAct1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ajswact1/AjswAct1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajswact1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjswAct1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
